fe8ed2a5c7fbddd7450487e7611a41ace4d9e600
Carlos Sosa Rivero <81424281+sosarivero@users.noreply.github.com>
2024-03-16 23:36:19 +0000

    Finish removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
+function removeFromArray(arr, ...args) {
+    return arr.filter((item) => !args.includes(item));
+}
 
-};
+console.log(removeFromArray([1, 2, 3, 4], 5, 2, 3, 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
