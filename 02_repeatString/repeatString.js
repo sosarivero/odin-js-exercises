0433e278684e07bd26c8a89ef46a06dcd10a1962
Carlos Sosa Rivero <81424281+sosarivero@users.noreply.github.com>
2024-03-16 23:25:53 +0000

    Finish repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+function repeatString(str, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
 
-};
+  let returnStr = "";
+  
+  for (let i = 0; i < num; i++) {
+    returnStr += str;
+  }
+  return returnStr;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
