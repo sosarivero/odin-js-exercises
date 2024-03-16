1f9c7abc6c740d911ad39cb0de8ca9433b19d424
Carlos Sosa Rivero <81424281+sosarivero@users.noreply.github.com>
2024-03-16 23:29:06 +0000

    Finish reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
-};
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
 
 // Do not edit below this line
 module.exports = reverseString;
