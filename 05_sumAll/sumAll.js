d33510d33fb8431e48eceaea15a76c24efa11009
Carlos Sosa Rivero <81424281+sosarivero@users.noreply.github.com>
2024-03-16 23:49:31 +0000

    Finish sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,18 +1,19 @@
 function sumAll(a, b) {
-    const args = [a, b].sort();
-    for (argument of args) {
-        if (typeof argument != 'number' || argument < 0) {
-            return "ERROR";
-        }
+  const args = [a, b].sort();
+  for (argument of args) {
+    if (typeof argument != "number" || argument < 0) {
+      return "ERROR";
     }
-    const start = args[0];
-    const end = args[1];
-    const numbers = [];
-    for (let i = start; i <= end; i++) {
-        numbers.push(i);
-    }
+  }
+  const start = args[0];
+  const end = args[1];
 
-    return numbers.reduce((a, b) => a + b);
+  const numbers = [];
+  for (let i = start; i <= end; i++) {
+    numbers.push(i);
+  }
+
+  return numbers.reduce((a, b) => a + b);
 }
 
 // Do not edit below this line
