fcea35bdd16d8fe604d534c2a8183abf9fcc1df8
Carlos Sosa Rivero <81424281+sosarivero@users.noreply.github.com>
2024-03-17 00:12:19 +0000

    Finish tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const convertToCelsius = function() {
-};
+// Uses Unary operators because .toFixed() returns strings.
+// Also helps with the fact that the tests wanted whole numbers to not have decimals (.0)
 
-const convertToFahrenheit = function() {
-};
+function convertToCelsius(fahrenheitTemp) {
+  return +((fahrenheitTemp - 32) * 5 / 9).toFixed(1); 
+}
 
+function convertToFahrenheit(celsiusTemp) {
+  return +((celsiusTemp * 9 / 5) + 32).toFixed(1);
+}
+
+console.log(convertToCelsius(100))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
