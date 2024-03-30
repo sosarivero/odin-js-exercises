3e3c6dd77b59ddbcce6bb29214649a23d0e09e51
Carlos Sosa Rivero <81424281+sosarivero@users.noreply.github.com>
2024-03-30 08:32:30 +0000

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleObj) {
+  return peopleObj.reduce((a, b) => {
+    let aAge = getYearOfDeath(a.yearOfDeath) - a.yearOfBirth;
+    let bAge = getYearOfDeath(b.yearOfDeath) - b.yearOfBirth;
 
+    return aAge > bAge ? a : b;
+  });
+};
+
+function getYearOfDeath(num) {
+  if (num === undefined) {
+    let dateObj = new Date();
+    return dateObj.getFullYear();
+  } else {
+    return num;
+  }
 };
 
 // Do not edit below this line
