48f9e12d8a092c3dacfd8ce73c6be0fc1fc4e38d
Carlos Sosa Rivero <81424281+sosarivero@users.noreply.github.com>
2024-03-30 08:09:26 +0000

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  num = parseInt(num);
 
+  if (num < 0) return "OOPS";
+  if (num <= 1) return num;
+
+  return fibonacci(num - 2) + fibonacci(num - 1);
 };
 
 // Do not edit below this line
