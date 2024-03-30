2688b3fec137735f8f318a144ccac16173e458ff
Carlos Sosa Rivero <81424281+sosarivero@users.noreply.github.com>
2024-03-30 00:38:35 +0000

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // Converts to lowercase to handle cases.
+    // Then uses regex match. Matches all alphanumeric, skips punctuation
+    arr = string.toLowerCase().match(/[a-z0-9]/g);
+    // Array comparison is weird in JS. Better to compare stringified.
+    return arr.toString() === arr.reverse().toString();
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
